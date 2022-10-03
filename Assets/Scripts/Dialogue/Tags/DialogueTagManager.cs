cc9a5ad7dda7a4ecf90be986962c616302a29e9e
alfianAH <alfian.hamdani04@gmail.com>
2022-10-03 14:42:04 +0800

    Add dialogue manager and dialogue tag

---
+++ Assets/Scripts/Dialogue/Tags/DialogueTagManager.cs
@@ -0,0 +1,107 @@
+using System.Collections.Generic;
+using TheDuction.Dialogue.Illustrations;
+using TheDuction.Dialogue.Portraits;
+using TheDuction.Global;
+using TheDuction.Global.Effects;
+using TheDuction.SceneLoading;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TheDuction.Dialogue.Tags{
+    public class DialogueTagManager: SingletonBaseClass<DialogueTagManager>{
+        [SerializeField] private Image _blackScreen;
+
+        private DialogueManager _dialogueManager;
+        private DialogueIllustrationManager _dialogueIllustrationManager;
+        private DialoguePortraitManager _dialoguePortraitManager;
+
+        private void Awake(){
+            _dialogueManager = DialogueManager.Instance;
+            _dialogueIllustrationManager = DialogueIllustrationManager.Instance;
+            _dialoguePortraitManager = DialoguePortraitManager.Instance;
+        }
+
+        public void HandleTags(List<string> dialogueTags){
+            foreach (string dialogueTag in dialogueTags)
+            {
+                // Parse the tag
+                string[] splitTag = dialogueTag.Split(':');
+
+                if (splitTag.Length != 2)
+                {
+                    Debug.LogError("Tag could not be parsed: " + tag);
+                }
+
+                string tagKey = splitTag[0].Trim();
+                string tagValue = splitTag[1].Trim();
+                
+                // Handle tag
+                switch (tagKey)
+                {
+                    case DialogueTags.BGM_TAG:
+                        // BackgroundMusicManager.Instance.Play(tagValue);
+                        break;
+                    
+                    case DialogueTags.DIALOGUE_BOX_TAG:
+                        _dialogueManager.ShowOrHideDialogueBox(tagValue);
+                        break;
+                    
+                    case DialogueTags.ENDING_TAG:
+                        HandleEndingTag(tagValue);
+                        break;
+                    
+                    case DialogueTags.EVENT_TAG:
+                        SetEventData(tagValue);
+                        break;
+
+                    case DialogueTags.ILLUST_TAG:
+                        _dialogueManager.ShowOrHideDialogueBox(DialogueTags.BLANK_VALUE);
+                        _dialogueIllustrationManager.FileNames = tagValue;
+                        _dialogueIllustrationManager.Display();
+                        break;
+
+                    case DialogueTags.PORTRAIT_TAG:
+                        _dialoguePortraitManager.FileNames = tagValue;
+                        _dialoguePortraitManager.Display();
+                        break;
+
+                    case DialogueTags.SFX_TAG:
+                        // SoundEffectManager.Instance.Play(tagValue);
+                        break;
+                    
+                    case DialogueTags.SPEAKER_TAG:
+                        _dialogueManager.HandleSpeakerTag(tagValue);
+                        break;
+                    
+                    default:
+                        Debug.LogError("Tag is not in the list: " + tag);
+                        break;
+                }
+            }
+        }
+
+        private void HandleEndingTag(string tagValue){
+            // Handle ending tag
+            SceneList.SceneNames().ForEach(sceneName =>
+            {
+                if (sceneName == tagValue)
+                {
+                    // TODO: Make scene load trigger
+                    // SceneLoadTrigger.Instance.LoadScene(tagValue);
+                    return;
+                }
+            });
+
+            if (tagValue != DialogueTags.CONFIRM_ENDING) return;
+
+            StartCoroutine(AlphaFadingEffect.FadeIn(_blackScreen,
+                fadingSpeed: 0.02f
+                // afterEffect: () => SceneLoadTrigger.Instance.LoadScene("HomeScene")
+            ));
+        }
+
+        private void SetEventData(string tagValue){
+
+        }
+    }
+}
