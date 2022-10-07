b7f9a6bfc7c0f98847fea923e88b96d8bc4db964
alfianAH <alfian.hamdani04@gmail.com>
2022-10-07 14:31:41 +0800

    Remove object pooling in dialogue illustration and portrait

--- Assets/Scripts/Dialogue/Illustrations/DialogueIllustrationManager.cs
+++ Assets/Scripts/Dialogue/Illustrations/DialogueIllustrationManager.cs
@@ -1,4 +1,3 @@
-using System.Collections.Generic;
 using TheDuction.Dialogue.Tags;
 using TheDuction.Global;
 using TheDuction.Global.Effects;
@@ -9,75 +8,38 @@ namespace TheDuction.Dialogue.Illustrations{
         SingletonBaseClass<DialogueIllustrationManager>, IDialoguePropertiesManager
     {
         [Header("Dialogue Illustration")]
-        [SerializeField] private Transform _illustrationParent;
-        [SerializeField] private DialogueIllustrationPrefab _illustrationPrefab;
+        [SerializeField] private DialogueIllustrationPrefab _illustrationObject;
 
-        private string _fileNames;
-        private List<DialogueIllustrationPrefab> _illustrationPool;
+        private string _fileName;
 
-        public string FileNames{
+        public string FileName{
             set{
-                _fileNames = value;
+                _fileName = value;
             }
-        }
-
-        private void Awake() {
-            _illustrationPool = new List<DialogueIllustrationPrefab>();
         }
 
         public void Display()
         {
-            string[] files = _fileNames.Split(',');
-            
             // If there are no portraits or none, hide portrait and return right away
-            if (files.Length <= 0 || _fileNames == DialogueTags.BLANK_VALUE)
+            if (_fileName == DialogueTags.BLANK_VALUE)
             {
                 Hide();
                 return;
             }
             
             Hide(); // Hide previous illustration
+            Sprite illustration = Resources.Load<Sprite>($"Illustrations/{_fileName}");
 
-            foreach (string filename in files)
-            {
-                Sprite illustration = Resources.Load<Sprite>($"Illustrations/{filename}");
-                DialogueIllustrationPrefab illustrationObject = GetOrCreateDialogueIllustrationObject();
-
-                illustrationObject.gameObject.SetActive(true);
-                illustrationObject.IllustrationSprite = illustration;
-                illustrationObject.PrefabSetup();
-            }
+            _illustrationObject.gameObject.SetActive(true);
+            _illustrationObject.IllustrationSprite = illustration;
+            _illustrationObject.PrefabSetup();
         }
 
         public void Hide()
         {
-            foreach (DialogueIllustrationPrefab illustration in _illustrationPool)
-            {
-                StartCoroutine(AlphaFadingEffect.FadeOut(illustration.IllustrationImage,
-                    afterEffect: () => illustration.gameObject.SetActive(false))
-                );
-            }
-        }
-
-        /// <summary>
-        /// Illustration object pooling
-        /// </summary>
-        /// <returns>Return existing illustration in hierarchy or create a new one</returns>
-        private DialogueIllustrationPrefab GetOrCreateDialogueIllustrationObject()
-        {
-            DialogueIllustrationPrefab dialogueIllustration = _illustrationPool.Find(illustrtation => 
-                !illustrtation.gameObject.activeInHierarchy);
-
-            if (dialogueIllustration == null)
-            {
-                dialogueIllustration = Instantiate(_illustrationPrefab, _illustrationParent).GetComponent<DialogueIllustrationPrefab>();
-                // Add new choice object to pool 
-                _illustrationPool.Add(dialogueIllustration);
-            }
-            
-            dialogueIllustration.gameObject.SetActive(false);
-
-            return dialogueIllustration;
+            StartCoroutine(AlphaFadingEffect.FadeOut(_illustrationObject.IllustrationImage,
+                afterEffect: () => _illustrationObject.gameObject.SetActive(false))
+            );
         }
     }
 }
