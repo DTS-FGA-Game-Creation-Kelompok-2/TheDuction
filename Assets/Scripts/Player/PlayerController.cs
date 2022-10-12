b5c04a863de8c9f69ba0dcc8ac6f30f95d221901
Alfian Aldy Hamdani <alfian.hamdani04@gmail.com>
2022-10-12 20:25:50 +0800

    Add prefab for managers and combine player controller but still bug

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,9 +5,14 @@ namespace TheDuction.Player
 {
     public class PlayerController : MonoBehaviour
     {
-        [SerializeField] private float _moveSpeed;
+        [SerializeField] private float _moveSpeed = 10f;
+
+        [SerializeField] private Animator _animator;
         private Rigidbody _rb;
-        
+        private bool _isWalking = false;
+
+        private const string IS_WALKING_PARAMETER = "isMoving";
+
         private void OnEnable()
         {
             InputConfig.OnInput += Move;
@@ -23,6 +28,16 @@ namespace TheDuction.Player
             _rb = GetComponent<Rigidbody>();
         }
 
+        private void Update(){
+            Debug.Log(_rb.velocity);
+            Debug.Log("X = " + Mathf.Approximately(_rb.velocity.x, 0.0f));
+            Debug.Log("Z = " + (_rb.velocity.z == 0.0f));
+            if(_rb.velocity.x == 0.0f && _rb.velocity.z == 0.0f){
+                _isWalking = false;
+            }
+            _animator.SetBool(IS_WALKING_PARAMETER, _isWalking);
+        }
+
         private void Move(Vector3 dir)
         {
             Vector3 movement = new Vector3(dir.x, 0, dir.z).normalized;
@@ -30,6 +45,7 @@ namespace TheDuction.Player
 
             _rb.velocity = movement * _moveSpeed;
             _rb.MoveRotation(rotation);
+            _isWalking = true;
         }
     }
 }
