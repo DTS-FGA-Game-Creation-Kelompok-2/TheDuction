76d4321365aacf78d07ac6191bea8c8ca971d80d
Abdul003 <73937383+nemesisx18@users.noreply.github.com>
2022-10-20 07:54:47 +0700

    update tutorial manager, testing switching between tutorial state

--- Assets/Scripts/UI/Tutorial/TutorialManager.cs
+++ Assets/Scripts/UI/Tutorial/TutorialManager.cs
@@ -1,10 +1,10 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialManager : MonoBehaviour
 {
+    [SerializeField] private TutorialState _tutorialState;
+    
     public enum TutorialState
     {
         Move,
@@ -12,19 +12,48 @@ public class TutorialManager : MonoBehaviour
         Inventory,
         End
     }
-    
+
+    private void Update()
+    {
+        TutorialStateChange(_tutorialState);
+    }
+
     private void TutorialStateChange(TutorialState newState)
     {
         switch (newState)
         {
             case TutorialState.Move:
+                Debug.Log("Move tutorial");
                 break;
             case TutorialState.Interact:
+                Debug.Log("Interact tutorial");
                 break;
             case TutorialState.Inventory:
+                Debug.Log("Inventory tutorial");
                 break;
             case TutorialState.End:
+                Debug.Log("End tutorial");
                 break;
         }
+    }
+
+    private void InitMoveTutorial()
+    {
+        TutorialStateChange(TutorialState.Move);
+    }
+    
+    private void InitInteractTutorial()
+    {
+        TutorialStateChange(TutorialState.Interact);
+    }
+    
+    private void InitInventoryTutorial()
+    {
+        TutorialStateChange(TutorialState.Inventory);
+    }
+    
+    private void InitEndTutorial()
+    {
+        TutorialStateChange(TutorialState.End);
     }
 }
